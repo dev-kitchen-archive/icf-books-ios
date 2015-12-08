811a49e658f928aeb86d69090aa1087e98de6503
Andreas Plüss <pluesand@students.zhaw.ch>
2015-12-08 19:47:04 +0100

    setup raw prototype app with tabed layout

---
+++ interactive book/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  interactive book
+//
+//  Created by Andreas Plüss on 08.12.15.
+//  Copyright © 2015 devkitchen. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
