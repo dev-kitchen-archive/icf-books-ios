4b0ff220d0f6f38f0845e22350f352019965cff9
Andreas Plüss <andreas@qlu.ch>
2015-12-15 22:40:21 +0100

    custom font for cell title and less aggressive colors

--- interactive book/StartPageCell.swift
+++ interactive book/StartPageCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class StartPageCell: UITableViewCell {
+    @IBOutlet weak var contentContainerView: UIView!
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var cellTitle: UILabel!
     @IBOutlet weak var cellDescription: UILabel!
@@ -16,12 +17,28 @@ class StartPageCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        cellTitle.font = UIFont(descriptor: UIFontDescriptor(name: "AmericanTypewriter", size: 18), size: 18)
+        cellTitle.textColor = UIColor(red:1, green:0.4, blue:0.38, alpha:1)
+        
+        contentContainerView.layer.borderWidth = 0.5
+        contentContainerView.layer.borderColor = UIColor(red:1, green:0.4, blue:0.38, alpha:1).CGColor
+        
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func applyPlainShadow(view: UIView) {
+        var layer = view.layer
+        
+        layer.shadowColor = UIColor.blackColor().CGColor
+        layer.shadowOffset = CGSize(width: 0, height: 10)
+        layer.shadowOpacity = 0.4
+        layer.shadowRadius = 5
     }
 
 }
