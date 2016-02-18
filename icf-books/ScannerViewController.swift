ff61dc51ac386df067450579146f7b4734ad57e5
Andreas Plüss <andreas@qlu.ch>
2016-02-18 11:54:49 +0100

    finalised camera view and modal transition

---
+++ icf-books/ScannerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ScannerViewController.swift
+//  icf-books
+//
+//  Created by Andreas Plüss on 18.02.16.
+//  Copyright © 2016 devkitchen. All rights reserved.
+//
+
+import UIKit
+
+class ScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonPress(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
