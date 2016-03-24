2d1ad8afeed3a46d6ef898ccb75a67a5ea6ccbba
Andreas Plüss <andreas@qlu.ch>
2016-03-24 23:12:18 +0100

    added acent color to navigation buttons, controls and titles

---
+++ icf-books/NavigationController.swift
@@ -0,0 +1,38 @@
+//
+//  NavigationController.swift
+//  icf-books
+//
+//  Created by Andreas Plüss on 24.03.16.
+//  Copyright © 2016 devkitchen. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    let accentColor = Color.accent
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationBar.tintColor = accentColor
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
